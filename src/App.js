daabb6cd491aa4036cce64abaf8be9cb41dad1b8
ianbuen <ianbuen@gmail.com>
2023-02-10 13:28:47 +0800

    edit attribution

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import ExpensesChart from "./components/ExpensesChart";
 function App() {
   return (
     <div className="app">
-      <ExpensesChart />
+      <main>
+        <ExpensesChart />
+      </main>
+
+      <footer>
+        <div class="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+          Coded by <a href="https://frontendmentor.io/profile/ianbuen" target="_blank" rel="noreferrer">@ianbuen</a>.
+        </div>
+      </footer>
     </div>
   );
 }
